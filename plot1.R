eab3a973cedfd2e614638806616a4eaa85df1c74
donaldmak0910 <34604527+donaldmak0910@users.noreply.github.com>
2020-07-12 00:44:02 +0800

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,24 @@
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+power <- read.table(unz(temp,"household_power_consumption.txt"), 
+                    sep=";", 
+                    header = T, 
+                    na="?", 
+                    colClasses = c("character",
+                                   'character',
+                                   'numeric',
+                                   'numeric',
+                                   'numeric',
+                                   'numeric',
+                                   'numeric',
+                                   'numeric',
+                                   'numeric'))
+
+unlink(temp)
+
+
+power <- power[which(power$Date == '2/2/2007' | power$Date=='1/2/2007'),]
+
+png(filename="plot1.png",width=480, height=480)
+hist(power$Global_active_power,main="Global Active Power",xlab = "Global Active Power (kilowatts)",col="red")
+dev.off()
